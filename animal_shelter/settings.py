013bf5584524777fa7a47032f935c89686a55e3d
KI-Shadow <ivankozlitin2012@gmail.com>
2020-11-17 13:47:06 +0300

    Site completed

--- animal_shelter/settings.py
+++ animal_shelter/settings.py
@@ -76,16 +76,12 @@ WSGI_APPLICATION = 'animal_shelter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-import dj_database_url  
-	DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
